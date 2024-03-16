0968249ca2ca4c5456bdb2253146ed01108f532b
comred <comred01@yandex.ru>
2024-03-16 23:45:48 +0500

    123
    123

--- data.js
+++ data.js
@@ -1,12 +1,31 @@
-export let json = {
+let datajs = {
     name: 123,
     age: 23,
     text: 'Lorem "Lore"m" i@#sum dolor sit amet, /b <p>123</p> consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quam et eum id ad quia, rerum porro. Obcaecati minus, quaerat necessitatibus labore quisquam hic harum, minima iste vel dicta aliquid!',
     test: {
-        "name1": "name10",
-        "name2": "name20",
-        "name3": "name30",
-        "name4": "name40"
+        name1: "name10",
+        name2: "name20",
+        name3: "name30",
+        name4: "name40"
     },
+    masstest: [
+        {
+            name: 'tom',
+            age: '24'
+        },
+        {
+            name: 'bob',
+            age: '31'
+        },
+        {
+            name: 'mike',
+            age: '15'
+        },
+    ],
     img: "./image/orig1.webp"
 }
+
+let nod = '12'
+
+export {datajs, nod}
+
